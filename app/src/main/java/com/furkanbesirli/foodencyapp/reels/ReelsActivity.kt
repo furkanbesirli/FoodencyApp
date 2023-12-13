9935d795880610618ad82d439e9e192b4c30ddfa
furkanbesirli <frknnnbesirli@gmail.com>
2023-12-13 04:01:21 +0300

    Profile ve Video ekleme ekranları üzerinde çalışıldı.

--- app/src/main/java/com/furkanbesirli/foodencyapp/reels/ReelsActivity.kt
+++ app/src/main/java/com/furkanbesirli/foodencyapp/reels/ReelsActivity.kt
@@ -8,5 +8,8 @@ class ReelsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reels)
+
+
+
     }
 }
